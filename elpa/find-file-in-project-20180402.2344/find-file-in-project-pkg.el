feeaeba76d636221ecf8b317c55306d99f29dbb1
teeaychem <ben@sparkes.uk.net>
2018-04-05 10:08:41 -0700

    check in

---
+++ elpa/find-file-in-project-20180402.2344/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20180402.2344" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.3")) :commit "69ec39f0519743dad963d372fd40ea6e21f2eb1f" :keywords '("project" "convenience") :url "https://github.com/technomancy/find-file-in-project")
