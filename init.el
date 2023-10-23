324a3a4e599c71f70af8eb0dbc8ab52eb0cd4426
teeaychem <ben@sparkes.uk.net>
2023-10-23 21:47:31 +0100

    cleaning up some things and new completion setup

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
-;; magic. . .
+;; -*- lexical-binding: t; -*-
 
+;;; Code:
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
@@ -23,135 +24,47 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-amsmath-label "eq:")
- '(LaTeX-math-abbrev-prefix "C-c 1")
  '(TeX-command-list
    '(("LatexMk" "latexmk %(-PDF)%S%(mode) %(file-line-error) %(extraopts) %t" TeX-run-latexmk nil
       (plain-tex-mode latex-mode)
       :help "Run LatexMk")
-     ("TeX" "%(PDF)%(tex) %(file-line-error) %`%(extraopts) %S%(PDFout)%(mode)%' %(output-dir) %t" TeX-run-TeX nil
-      (plain-tex-mode texinfo-mode ams-tex-mode)
-      :help "Run plain TeX")
      ("LaTeX" "%`%l%(mode)%' %T" TeX-run-TeX nil
       (latex-mode doctex-mode)
       :help "Run LaTeX")
-     ("Makeinfo" "makeinfo %(extraopts) %(o-dir) %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with Info output")
-     ("Makeinfo HTML" "makeinfo %(extraopts) %(o-dir) --html %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with HTML output")
-     ("AmSTeX" "amstex %(PDFout) %`%(extraopts) %S%(mode)%' %(output-dir) %t" TeX-run-TeX nil
-      (ams-tex-mode)
-      :help "Run AMSTeX")
-     ("ConTeXt" "%(cntxcom) --once --texutil %(extraopts) %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt once")
-     ("ConTeXt Full" "%(cntxcom) %(extraopts) %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt until completion")
      ("BibTeX" "bibtex %(O?aux)" TeX-run-BibTeX nil
       (plain-tex-mode latex-mode doctex-mode context-mode texinfo-mode ams-tex-mode)
       :help "Run BibTeX")
      ("Biber" "biber %(output-dir) %s" TeX-run-Biber nil
       (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
       :help "Run Biber")
-     ("Texindex" "texindex %s.??" TeX-run-command nil
-      (texinfo-mode)
-      :help "Run Texindex")
-     ("Texi2dvi" "%(PDF)texi2dvi %t" TeX-run-command nil
-      (texinfo-mode)
-      :help "Run Texi2dvi or Texi2pdf")
      ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
-     ("Print" "%p" TeX-run-command t t :help "Print the file")
-     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
-     ("File" "%(o?)dvips %d -o %f " TeX-run-dvips t
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Generate PostScript file")
-     ("Dvips" "%(o?)dvips %d -o %f " TeX-run-dvips nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert DVI file to PostScript")
-     ("Dvipdfmx" "dvipdfmx -o %(O?pdf) %d" TeX-run-dvipdfmx nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert DVI file to PDF with dvipdfmx")
-     ("Ps2pdf" "ps2pdf %f %(O?pdf)" TeX-run-ps2pdf nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Convert PostScript file to PDF")
-     ("Glossaries" "makeglossaries %(d-dir) %s" TeX-run-command nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run makeglossaries to create glossary file")
-     ("Index" "makeindex %(O?idx)" TeX-run-index nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run makeindex to create index file")
-     ("upMendex" "upmendex %(O?idx)" TeX-run-index t
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run upmendex to create index file")
-     ("Xindy" "texindy %s" TeX-run-command nil
-      (plain-tex-mode latex-mode doctex-mode texinfo-mode ams-tex-mode)
-      :help "Run xindy to create index file")
      ("Check" "lacheck %s" TeX-run-compile nil
       (latex-mode)
       :help "Check LaTeX file for correctness")
      ("ChkTeX" "chktex -v6 %s" TeX-run-compile nil
       (latex-mode)
       :help "Check LaTeX file for common mistakes")
-     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
      ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
      ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
      ("Other" "" TeX-run-command t t :help "Run an arbitrary command")))
  '(TeX-electric-escape nil)
- '(TeX-source-correlate-method '((dvi . source-specials) (pdf . synctex)))
  '(TeX-source-correlate-mode t)
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#000000" "#e74c3c" "#b6e63e" "#e2c770" "#268bd2" "#fb2874" "#66d9ef" "#ffffff"])
  '(custom-safe-themes
-   '("48441785d77807dce4e71b065f17504fbdfb6cbcd475523d20da4f1a14fbddac" "5acb6002127f5d212e2d31ba2ab5503df9cd1baa1200fbb5f57cc49f6da3056d" "a7e7804313dbf827a441c86a8109ef5b64b03011383322cbdbf646eb02692f76" "2e1d19424153d41462ad31144549efa41f55dacda9b76571f73904612b15fd0a" "0a3a41085c19d8121ed0ad3eb658a475ccb948a70a83604641ee7d4c3575a4d5" "a4d03266add9a1c8f12b5309612cbbf96e1291773c7bc4fb685bfdaf83b721c6" "599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "4a7abcca7cfa2ccdf4d7804f1162dd0353ce766b1277e8ee2ac7ee27bfbb408f" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "5900bec889f57284356b8216a68580bfa6ece73a6767dfd60196e56d050619bc" "d507c9e58cb0eb8508e15c8fedc2d4e0b119123fab0546c5fd30cadd3705ac86" "9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "365d9553de0e0d658af60cff7b8f891ca185a2d7ba3fc6d29aadba69f5194c7f" default))
- '(fci-rule-color "#555556")
+   '("88cb0f9c0c11dbb4c26a628d35eb9239d1cf580cfd28e332e654e7f58b4e721b" default))
  '(flycheck-checker-error-threshold 99400)
  '(font-latex-fontify-script nil)
- '(hl-sexp-background-color "#efebe9")
- '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#fd971f"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#b6e63e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#525254"))
  '(mac-command-modifier 'super)
  '(mac-right-option-modifier 'left)
  '(mac-wheel-button-is-mouse-2 nil)
- '(org-agenda-files '("~/Documents/projects/SICP/exercises.org"))
  '(org-fontify-done-headline t)
  '(org-fontify-quote-and-verse-blocks t)
  '(org-fontify-whole-heading-line t)
  '(org-list-allow-alphabetical t)
- '(org-preview-latex-image-directory "~/OrgTemp/")
+ '(org-preview-latex-imaqge-directory "~/OrgTemp/")
  '(package-selected-packages
-   '(racket-mode undo-tree web-mode dracula-theme smartparens slime diff-hl highlight-indent-guides fix-word no-littering fzf elpy magit yasnippet bind-key diminish s popwin popup epl pkg-info math-symbol-lists async dash company helm-core which-key helm-company helm smart-mode-line-powerline-theme use-package exec-path-from-shell smart-mode-line multiple-cursors browse-kill-ring rainbow-delimiters company-math flycheck company-auctex auctex-latexmk auctex))
- '(preview-image-type 'dvipng)
- '(preview-scale-function 1.0)
- '(reftex-cite-prompt-optional-args t)
+   '(inkpot-theme modus-themes racket-mode undo-tree web-mode dracula-theme smartparens slime diff-hl highlight-indent-guides fix-word no-littering fzf elpy magit yasnippet bind-key diminish s popwin popup epl pkg-info math-symbol-lists async dash company which-key smart-mode-line-powerline-theme use-package exec-path-from-shell smart-mode-line multiple-cursors browse-kill-ring rainbow-delimiters company-math flycheck company-auctex auctex-latexmk auctex))
  '(undo-tree-history-directory-alist
-   '(("" . "/Users/sparkes/Library/CloudStorage/Dropbox/Symlink/.emacs.d/undoTree")))
- '(vc-annotate-background "#000000")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#b6e63e")
-    (cons 40 "#c4db4e")
-    (cons 60 "#d3d15f")
-    (cons 80 "#e2c770")
-    (cons 100 "#ebb755")
-    (cons 120 "#f3a73a")
-    (cons 140 "#fd971f")
-    (cons 160 "#fc723b")
-    (cons 180 "#fb4d57")
-    (cons 200 "#fb2874")
-    (cons 220 "#f43461")
-    (cons 240 "#ed404e")
-    (cons 260 "#e74c3c")
-    (cons 280 "#c14d41")
-    (cons 300 "#9c4f48")
-    (cons 320 "#77504e")
-    (cons 340 "#555556")
-    (cons 360 "#555556")))
- '(vc-annotate-very-old-color nil))
+   '(("" . "/Users/sparkes/Library/CloudStorage/Dropbox/Symlink/.emacs.d/undoTree"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -162,21 +75,18 @@
  '(font-latex-superscript-face ((t nil)))
  '(hl-line ((t (:background "#F6FECD" :underline nil)))))
 
-(load "server")
+(require 'server)
 (unless (server-running-p) (server-start)) ; start emacs in server mode so that skim can talk to it
 
-
 ;; This should always be at the end!
-;;    Set alt to meta
-;;    (setq mac-option-modifier 'meta)
 ;; Here we set cmd to meta, leaving alt available for alternative characters.
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
+;; (setq mac-option-key-is-meta nil)
+;; (setq mac-command-key-is-meta t)
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 ;; Set fn to hyper
-(setq ns-function-modifier 'hyper)
+;; (setq ns-function-modifier 'hyper)
 ;; Also, don't pass uses of command to the system
 (setq mac-pass-command-to-system nil)
 
-;; init.el ends here
+;;; init.el ends here
