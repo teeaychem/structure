b846c32642180cb543b934cb3ca59a31e3b818a0
teeaychem <ben@sparkes.uk.net>
2024-03-03 17:26:29 +0000

    Remove some unnecessary things

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,9 +16,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(package-selected-packages
-	 '(toc-org racket-mode undo-tree web-mode dracula-theme slime diff-hl highlight-indent-guides fix-word no-littering fzf magit yasnippet bind-key diminish s popwin popup epl pkg-info math-symbol-lists async dash which-key smart-mode-line-powerline-theme use-package exec-path-from-shell smart-mode-line multiple-cursors browse-kill-ring rainbow-delimiters flycheck auctex-latexmk auctex)))
+   '(puni doom-themes racket-mode undo-tree web-mode slime diff-hl highlight-indent-guides fix-word no-littering fzf magit yasnippet bind-key diminish s popwin dash which-key smart-mode-line-powerline-theme exec-path-from-shell smart-mode-line multiple-cursors browse-kill-ring rainbow-delimiters auctex-latexmk auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
