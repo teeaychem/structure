a3d1360e010bfdd775c0172772aaae3a1a30652d
teeaychem <ben@sparkes.uk.net>
2024-05-26 23:04:47 +0100

    move inside .config
    some other misc changes, and a reworked .gitignore

---
+++ emacs/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("64045b3416d83e5eac0718e236b445b2b3af02ff5bcd228e9178088352344a92" "c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" default))
+ '(package-selected-packages
+   '(vc-use-package ellama eglot-booster tempel which-key vertico undo-tree treesit-auto rustic rainbow-delimiters py-autopep8 pasp-mode orderless no-littering multiple-cursors marginalia magit lua-mode highlight-indent-guides fix-word exec-path-from-shell doom-themes diff-hl corfu consult-project-extra clang-format cape browse-kill-ring auctex-latexmk))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package")))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
