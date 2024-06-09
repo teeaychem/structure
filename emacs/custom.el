e9efc4da74131dec7753c50a336f1843fd7fcf40
teeaychem <ben@sparkes.uk.net>
2024-06-09 12:15:33 +0100

    Remove rustic, fix tempel, a new bind, some ignorance

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("64045b3416d83e5eac0718e236b445b2b3af02ff5bcd228e9178088352344a92" "c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" default))
  '(package-selected-packages
-   '(undo-fu-session undo-fu vundo eglot-x vc-use-package eglot-booster tempel which-key vertico treesit-auto rustic rainbow-delimiters py-autopep8 pasp-mode orderless no-littering multiple-cursors marginalia magit lua-mode highlight-indent-guides fix-word exec-path-from-shell doom-themes diff-hl corfu consult-project-extra clang-format cape browse-kill-ring auctex-latexmk))
+   '(undo-fu-session undo-fu vundo eglot-x vc-use-package eglot-booster tempel which-key vertico treesit-auto rainbow-delimiters py-autopep8 pasp-mode orderless no-littering multiple-cursors marginalia magit lua-mode highlight-indent-guides fix-word exec-path-from-shell doom-themes diff-hl corfu consult-project-extra clang-format cape browse-kill-ring auctex-latexmk))
  '(package-vc-selected-packages
    '((vundo :vc-backend Git :url "https://github.com/casouri/vundo")
      (eglot-x :vc-backend Git :url "https://github.com/nemethf/eglot-x")
