c9e701f387fc957f40e3f1a02ad20986febdf10c
teeaychem <ben@sparkes.uk.net>
2024-09-19 14:26:23 +0100

    some use-package details, and back to 29
    though, it seems git-grep issue were due to lsp-mode rather than 30
    
    main addition is gnu-devel for the latest eglot, but also setting priorities with elpa at 0, gnu-devel at -1 and melpa-stable at 1 for a strong preference to stable

--- emacs/init.el
+++ emacs/init.el
@@ -1,18 +1,39 @@
 ;; -*- lexical-binding: t; -*-
 
 ;;; Code:
-(require 'package)
-(package-initialize)
-(setq package-enable-at-startup nil) ;; don't make installed packages available before loading the init.el file.
-(setq use-package-always-ensure t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-
-(setq vc-follow-symlinks t) ;; always open the file a symlink points to
 
 ;; save custom things to separate file
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
+
+(setq package-archive-priorities
+      '(("melpa-stable" . 1)
+        ("melpa" . 0)
+        ("gnu-devel" . -1)))
+
+;; load custom.el before doing anything else with packages as it contains a useful list
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+(package-initialize)
+
+
+(when (not (file-directory-p (expand-file-name "elpa" user-emacs-directory)))
+  (package-refresh-contents)
+  (package-install-selected-packages))
+
+(setq use-package-verbose t)
+(setq package-enable-at-startup nil) ;; don't make installed packages available before loading the init.el file.
+(setq use-package-always-ensure t)
+
+
+
+(setq vc-follow-symlinks t) ;; always open the file a symlink points to
+
+
 
 
 ;; Load config.org - my Emacs config
